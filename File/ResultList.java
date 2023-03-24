0e6258b377c876afbe9eb3490928da0508ce7df6
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2023-03-24 15:33:47 +0700

    fixed

--- File/ResultList.java
+++ File/ResultList.java
@@ -4,17 +4,14 @@ public class ResultList {
     public static void main(String[] args) {
         System.out.println(
             """
-            Test Result:
-                Subject                 Mark
-                OOP                     80
-                IPE                     81
-                Database                78
-                Automata                76
-                Web Design              84
-                Matlab                  70
-                Theory of Information   77
-            ----------------------------------
-                Average                 78
+                Test Result:
+                Category                Score
+                WPM                     40
+                acc                     88%
+                raw                     49
+                characters              99/7/3/0
+                consistency             69%
+                time                    31s
             """
         );
     }
