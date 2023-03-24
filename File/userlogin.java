554c3e41df466cb980ef79c5655f05dfb27c6ed3
serlong <128038147+serlong@users.noreply.github.com>
2023-03-24 21:48:04 +0700

    Add files via upload

---
+++ File/userlogin.java
@@ -0,0 +1,17 @@
+package source;
+
+import java.util.Scanner;
+
+public class userlogin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter name: ");
+        String name = sc.nextLine();
+        System.out.printf("Enter password: ");
+        String pass = sc.nextLine();
+        String pass1 = pass;
+        if(pass == pass1){
+            System.out.printf("Login successful, Welcome " + name);
+        }
+    }
+}
