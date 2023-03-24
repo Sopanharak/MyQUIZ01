6551e927c616e495c92c4af655944d5f0cc61972
spidernak <127380223+spidernak@users.noreply.github.com>
2023-03-24 14:24:33 +0700

    Add files via upload

---
+++ File/Register.java
@@ -0,0 +1,19 @@
+package TypingSpeed;
+
+ 
+import java.util.Scanner;
+
+public class Register {
+ public static void main(String[] args) {
+    String name;String email; String password;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Please enter your name: ");
+    name = sc.nextLine();
+    System.out.print("Please enter your emaial: ");
+    email = sc.nextLine();
+    System.out.print("Please eneter your password: ");
+    password = sc.nextLine();
+ }
+    }
+
+ 
